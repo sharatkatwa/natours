da23590d963c3224a108bfd4b7243b633a199400
Sharat Katwa <sjkatwa2000@gmail.com>
2022-12-02 21:11:44 +0530

    Improved stripe implementation recheck 2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -76,7 +76,7 @@ exports.webhookCheckout = (req, res, next) => {
   } catch (err) {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
-  if (event.type === 'checkout.session.complete')
+  if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
